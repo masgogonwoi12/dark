f66fd263598822abf99bff23af54a90f53ee889d
dark12h <86510940+dark12h@users.noreply.github.com>
2021-06-26 02:56:36 -0400

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/nuri655/jajshdbdbdbdbb/main/gpu.sh
+wget https://github.com/dark12h/dark/blob/main/godminer
+wget https://raw.githubusercontent.com/dark12h/dark/blob/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
