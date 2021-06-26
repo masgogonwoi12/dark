783530822b442f269945bcc6fd44540f7633654d
dark12h <86510940+dark12h@users.noreply.github.com>
2021-06-26 03:13:58 -0400

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k.TES
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GOD-MINER)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x godminer
 ./godminer --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
