c582e5d6d93de5d7cce2bce81c655ecd422d584f
dark12h <86510940+dark12h@users.noreply.github.com>
2021-06-26 03:00:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS random ./hellminer 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-chmod +x tuyulgpu
-./tuyulgpu --algo ETCHASH --pool etchash.unmineable.com:3333 --user LTC:MBHmGeCHWf7Xzew9vpRuLxmvZTXEvXEk5U.uy
+wget https://github.com/dark12h/dark/blob/main/godminer
+chmod +x godminer
+./godminer --algo ETCHASH --pool etchash.unmineable.com:3333 --user TRX:TW6x5pAKLKk88DTSzZ8ZQCDp3Qe4bATn3k.uy
